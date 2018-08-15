64bcd3c701fb10c81d89ba6798617ffda9934c67
Ashwini Todkar <ashwini.todkar@accionlabs.com>
2018-08-15 23:43:54 +0530

    adding method to get factorial of number without recursion

--- recursion/factorial.js
+++ recursion/factorial.js
@@ -10,3 +10,17 @@ function factorial(n){
     console.log(n);
     return n * factorial(n-1);
 }
+
+/**
+ * Method to get factorial of given number without recursion
+ * Factorial of a number n is the product of all positive numbers less than equal to n
+ * @param {*} n 
+ */
+function factorial(n){
+    var fact = 1;
+    while(n > 0){
+        fact = fact * n;
+        n = n - 1;
+    }
+    return fact;
+}
